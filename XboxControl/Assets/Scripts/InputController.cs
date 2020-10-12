2453d708ade3c3c55b73aea9b78b5876013412d3
hanstheman <hey2k_bad@hushmail.com>
2020-10-12 21:19:16 +0200

    triggers sending Actions!!

--- XboxControl/Assets/Scripts/InputController.cs
+++ XboxControl/Assets/Scripts/InputController.cs
@@ -30,23 +30,26 @@ namespace extOSC.Examples
         #endregion
         private void Update()
         {
+            ProcessInput();
+
+            #region old code
             // check input
-            if (Input.GetKeyDown("space"))
-            {
-                print("space pressed");
-                string newAddress = "/vkb_midi/1/note/70";
-                var message = new OSCMessage(newAddress);
-                message.AddValue(OSCValue.Int(1));
-                Transmitter.Send(message);
-            }
-            else if (Input.GetKeyUp("space"))
-            {
-                print("space released");
-                string newAddress = "i/vkb_midi/1/note/70";
-                var message = new OSCMessage(newAddress);
-                message.AddValue(OSCValue.Int(0));
-                Transmitter.Send(message);
-            }
+            //if (Input.GetKeyDown("space"))
+            //{
+            //    print("space pressed");
+            //    string newAddress = "/vkb_midi/1/note/70";
+            //    var message = new OSCMessage(newAddress);
+            //    message.AddValue(OSCValue.Int(1));
+            //    Transmitter.Send(message);
+            //}
+            //else if (Input.GetKeyUp("space"))
+            //{
+            //    print("space released");
+            //    string newAddress = "i/vkb_midi/1/note/70";
+            //    var message = new OSCMessage(newAddress);
+            //    message.AddValue(OSCValue.Int(0));
+            //    Transmitter.Send(message);
+            //}
 
             // pitch bend wheel
             //float newVal = Input.GetAxis("Horizontal");
@@ -58,17 +61,17 @@ namespace extOSC.Examples
             //Transmitter.Send(pitchMessage);
 
             // -1 to 1
-            Debug.Log("Joy 1 horizontal : " + Input.GetAxis("Horizontal"));
-            Debug.Log("Joy 1 vertical : " + Input.GetAxis("Vertical"));
-            Debug.Log("Joy 2 horizontal : " + Input.GetAxis("Horizontal2"));
-            Debug.Log("Joy 2 vertical : " + Input.GetAxis("Vertical2"));
-            // remap to midi cc, 0 to 127 : + 1 , * 64 , + 64 
-            float joy1Horizontal = (Input.GetAxis("Horizontal") + 1) * 64; // TODO now 128 max ?
-            Debug.Log("Joy 1 horizontal in midi : " + joy1Horizontal);
-            string newCCAddress = "f/vkb_midi/1/cc/80";
-            var joy1HorizontalMessage = new OSCMessage(newCCAddress);
-            joy1HorizontalMessage.AddValue(OSCValue.Float(joy1Horizontal));
-            Transmitter.Send(joy1HorizontalMessage);
+            //Debug.Log("Joy 1 horizontal : " + Input.GetAxis("Horizontal"));
+            //Debug.Log("Joy 1 vertical : " + Input.GetAxis("Vertical"));
+            //Debug.Log("Joy 2 horizontal : " + Input.GetAxis("Horizontal2"));
+            //Debug.Log("Joy 2 vertical : " + Input.GetAxis("Vertical2"));
+            //// remap to midi cc, 0 to 127 : + 1 , * 64 , + 64 
+            //float joy1Horizontal = (Input.GetAxis("Horizontal") + 1) * 64; // TODO now 128 max ?
+            //Debug.Log("Joy 1 horizontal in midi : " + joy1Horizontal);
+            //string newCCAddress = "f/vkb_midi/1/cc/80";
+            //var joy1HorizontalMessage = new OSCMessage(newCCAddress);
+            //joy1HorizontalMessage.AddValue(OSCValue.Float(joy1Horizontal));
+            //Transmitter.Send(joy1HorizontalMessage);
 
             // only send midi cc if change!!
 
@@ -77,13 +80,92 @@ namespace extOSC.Examples
             # The feedback values REAPER sends are identical to those sent for binary 
             # arguments.
             */
-            string newOSCAddress = "/play";
-            if (Input.GetButtonDown("Fire1"))
+            //string newOSCAddress = "/play";
+            //if (Input.GetButtonDown("Fire1"))
+            //{
+            //    Transmitter.Send(new OSCMessage(newOSCAddress));
+            //}
+            #endregion
+
+
+        }
+
+        private void ProcessInput() // slow down to 100ms, gather data in 1 package
+        {
+            StartCoroutine(ProcessTriggers());
+            ProcessShoulderBtns();
+           
+
+            // play and stop on cursor OR stop and rewind
+            // Left/Right Shoulder
+
+            // zoom in/out, 
+            // Left Thumbstick
+
+            // track down/up,  
+            // DPad up/down
+            // select previous/next mediaclip
+            // DPad left/right
+
+
+            // btn X
+            // cut at cursor (and selected track)
+
+            IEnumerator ProcessTriggers() // TODO add to package
             {
-                Transmitter.Send(new OSCMessage(newOSCAddress));
+                // TODO control speed by amount of calls per second - and 1ms or 5ms OR by beat or by measure (set in GUI)
+
+                // only want 1 active at a time : left dominant
+                float leftTrigger = Input.GetAxis("TriggerLeft_Axis");
+                float rightTrigger = Input.GetAxis("TriggerRight_Axis");
+                string Address = "/action/str";
+                var message = new OSCMessage(Address);
+
+                // max = -1 , min = 0
+                if(leftTrigger < 0) 
+                {
+                    print("left trigger : " + leftTrigger);
+                    // rewind, frame independent
+                    // Action call
+                    
+                    if(leftTrigger > -.25)
+                    {
+                        message.AddValue(OSCValue.String("_SWS_MOVECUR1MSLEFT"));
+
+                    }
+                    else if(leftTrigger > -.5)
+                    {
+                        message.AddValue(OSCValue.String("_SWS_MOVECUR5MSLEFT"));
+                    }
+                    else if(leftTrigger > -.75)
+                    {
+                        //Move edit cursor back one beat
+                        // add timeout
+                        message.AddValue(OSCValue.String("41045"));
+
+                    }
+                }
+                else if(rightTrigger < 0)
+                {
+                    print("right trigger : " + rightTrigger);
+                }
+
+
+                Transmitter.Send(message);
+                yield return new WaitForSeconds(.5f);
+
             }
 
 
+
+
+
+
+            void ProcessShoulderBtns()
+            {
+
+
+            }
 
         }
     }
